759c95ae9f552793ec0523d6d3697c28e2bde253
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-03-27 21:43:54 +0800

    Update EUdig9.sh

--- EUdig9.sh
+++ EUdig9.sh
@@ -6,9 +6,9 @@ apt install net-tools iproute2 openresolv dnsutils -y
 apt install wireguard-tools --no-install-recommends
 cp wireguard-go /usr/bin
 cp wgcf /usr/local/bin/wgcf
-chmod +x /usr/bin/wireguard-go wgcf
-echo | ./wgcf register
-./wgcf generate
+chmod +x /usr/bin/wireguard-go /usr/local/bin/wgcf
+echo | wgcf register
+wgcf generate
 sed -i 's/engage.cloudflareclient.com/2606:4700:d0::a29f:c001/g' wgcf-profile.conf
 sed -i '/\:\:\/0/d' wgcf-profile.conf
 cp wgcf-profile.conf /etc/wireguard/wgcf.conf
