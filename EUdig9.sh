b776911d20d6fde19ca6ea28c6033c6719d66269
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-03-29 06:14:33 +0800

    Update EUdig9.sh

--- EUdig9.sh
+++ EUdig9.sh
@@ -5,7 +5,7 @@ apt install net-tools iproute2 openresolv dnsutils -y
 apt install wireguard-tools --no-install-recommends
 cp wireguard-go /usr/bin
 cp wgcf /usr/local/bin/wgcf
-chmod +x /usr/bin/wireguard-go /usr/local/bin/wgcf
+chmod +x /usr/local/bin/wgcf
 echo | wgcf register
 wgcf generate
 sed -i 's/engage.cloudflareclient.com/2606:4700:d0::a29f:c001/g' wgcf-profile.conf
@@ -16,3 +16,4 @@ ln -sf /lib/systemd/system/systemd-resolved.service /etc/systemd/system/dbus-org
 systemctl enable wg-quick@wgcf
 systemctl start wg-quick@wgcf
 echo 'precedence  ::ffff:0:0/96   100' | sudo tee -a /etc/gai.conf
+echo -e "完成了，可以 ping 1.1.1.1 试试看了\n如果没出问题，这里应该能显示你的IP：$(wget -qO- ip.ip.sb) <=如果这里是空的，大概是失败了"
