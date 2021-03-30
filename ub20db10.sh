d05959c814bf565f3ed78ffb9e2a10f0e65731ef
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-03-30 17:02:15 +0800

    Update ub20db10.sh

--- ub20db10.sh
+++ ub20db10.sh
@@ -7,7 +7,6 @@ apt install net-tools iproute2 openresolv dnsutils -y
 apt install wireguard-tools --no-install-recommends
 curl -fsSL git.io/wireguard-go.sh | sudo bash
 curl -fsSL git.io/wgcf.sh | sudo bash
-chmod +x /usr/local/bin/wgcf
 echo | wgcf register
 wgcf generate
 sed -i 's/engage.cloudflareclient.com/2606:4700:d0::a29f:c001/g' wgcf-profile.conf
