fcad504987316be6f3ca23239858f9374408511b
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-03-31 20:30:29 +0800

    Update warp2.sh

--- warp2.sh
+++ warp2.sh
@@ -19,5 +19,5 @@ cp wgcf-profile.conf /etc/wireguard/wgcf.conf
 systemctl enable wg-quick@wgcf
 systemctl start wg-quick@wgcf
 echo 'precedence  ::ffff:0:0/96   100' | sudo tee -a /etc/gai.conf
-echo -e "检测是否优先启动Warp IPV4地址：如果下方显示为8.2X……开头的IPV4地址，就说明成功啦！……否则，再重新运行脚本吧"
+echo -e "检测是否优先启动Warp IPV4地址：如果下方显示为8.2X……开头的IPV4地址，就说明成功啦！……如果是2a02开头的IPV6地址，那就再重新运行脚本吧"
 curl ip.p3terx.com
