c831f6f8aea60264ae48fed0ccba5313744d1ad9
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-04-04 07:10:10 +0800

    Update and rename srvDIG91.sh to srvDIG9.sh

--- srvDIG9.sh
+++ srvDIG9.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo -e "\033[31m 仅支持Ubuntu与Debain系统 \033[0m"
+echo -e "\033[31m 仅支持Ubuntu与Debain系统 \033[0m"
+echo -e "\033[31m 仅支持Ubuntu与Debain系统 \033[0m"
 apt update && apt install curl sudo lsb-release iptables -y
 echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | sudo tee /etc/apt/sources.list.d/backports.list
 apt update
@@ -15,9 +18,11 @@ wgcf generate
 sed -i 's/engage.cloudflareclient.com/2606:4700:d0::a29f:c001/g' wgcf-profile.conf
 sed -i '/\:\:\/0/d' wgcf-profile.conf
 cp wgcf-profile.conf /etc/wireguard/wgcf.conf
+ln -s /usr/bin/resolvectl /usr/local/bin/resolvconf
+ln -sf /lib/systemd/system/systemd-resolved.service /etc/systemd/system/dbus-org.freedesktop.resolve1.service
 systemctl enable wg-quick@wgcf
 systemctl start wg-quick@wgcf
 rm -f srvDIG9* wgcf* wireguard-go*
 grep -qE '^[ ]*precedence[ ]*::ffff:0:0/96[ ]*100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' | sudo tee -a /etc/gai.conf
-echo -e "\033[33m 检测是否优先启动Warp IPV4地址：下方显示为8.2X……的IPV4地址，就说明成功啦！……如果是2a02开头的IPV6地址（申请WGCF账户失败），请重复运行脚本吧，直到成功为止" \033[0m
+echo -e "\033[33m 检测是否优先启动Warp IPV4地址：下方显示为8.2X……的IPV4地址，就说明成功啦！……如果是2a02开头的IPV6地址（申请WGCF账户失败），请重复运行脚本吧，直到成功为止 \033[0m"
 curl ip.p3terx.com
