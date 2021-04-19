a464368018a77c0ffb6eadec6e7ed775bf759dad
YG-tsj <80431714+YG-tsj@users.noreply.github.com>
2021-04-19 12:14:39 +0800

    Update warp4.sh

--- warp4.sh
+++ warp4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "\033[1;36m 请 注 意！脚 本 仅 支 持 Ubuntu 与 Debain 系 统！\n 主要针对OpenVZ、LXC架构的IPV6 only VPS！Warp仅接管IPV4网络！！！ \033[0m"
+echo -e "\033[1;36m 请 注 意！脚 本 仅 支 持 Ubuntu 与 Debain 系 统！\n 主要针对OpenVZ、LXC架构的IPV6 only VPS！！！Warp仅接管IPV4网络！！！ \033[0m"
 apt update && apt install curl sudo lsb-release iptables -y
 echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | sudo tee /etc/apt/sources.list.d/backports.list
 apt update
@@ -21,4 +21,4 @@ systemctl start wg-quick@wgcf
 rm -f warp4* wgcf* wireguard-go*
 grep -qE '^[ ]*precedence[ ]*::ffff:0:0/96[ ]*100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' | sudo tee -a /etc/gai.conf
 echo -e "\033[1;33m 检测是否成功启动Warp！\n 显示IPV4地址：$(wget -qO- ipv4.ip.sb) \033[0m"
-echo -e "\033[1;32m 如上方显示IPV4地址：8.2…………，则说明成功啦！\n 如上方无IP显示,（说明申请WGCF账户失败），请“无限”重复运行该脚本吧，直到成功为止！！！ \033[0m"
+echo -e "\033[1;32m 如上方显示IPV4地址：8.…………，则说明成功啦！\n 如上方无IP显示,（说明申请WGCF账户失败），请“无限”重复运行该脚本吧，直到成功为止！！！ \033[0m"
